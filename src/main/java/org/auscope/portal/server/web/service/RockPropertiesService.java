7ed23cfcbe34056e60f2b6c466e5fe008d688b0a
comtesacristain <mick.sexton@gmail.com>
2016-04-18 15:23:40 +1000

    GPT-104 Add count repsonse and check BBOX

--- src/main/java/org/auscope/portal/server/web/service/RockPropertiesService.java
+++ src/main/java/org/auscope/portal/server/web/service/RockPropertiesService.java
@@ -1,7 +1,6 @@
 package org.auscope.portal.server.web.service;
 
 import java.io.InputStream;
-
 import org.apache.http.client.methods.HttpRequestBase;
 import org.auscope.portal.core.server.http.HttpServiceCaller;
 import org.auscope.portal.core.services.BaseWFSService;
@@ -9,6 +8,7 @@ import org.auscope.portal.core.services.PortalServiceException;
 import org.auscope.portal.core.services.methodmakers.WFSGetFeatureMethodMaker;
 import org.auscope.portal.core.services.methodmakers.WFSGetFeatureMethodMaker.ResultType;
 import org.auscope.portal.core.services.methodmakers.filter.FilterBoundingBox;
+import org.auscope.portal.core.services.responses.wfs.WFSCountResponse;
 import org.auscope.portal.core.services.responses.wfs.WFSTransformedResponse;
 import org.auscope.portal.core.xslt.WfsToKmlTransformer;
 import org.auscope.portal.mineraloccurrence.RockPropertiesFilter;
@@ -34,17 +34,12 @@ public class RockPropertiesService extends BaseWFSService {
 
 	public WFSTransformedResponse getRockProperties(String serviceUrl, String rockProperty, int maxFeatures,
 			FilterBoundingBox bbox) throws Exception {
-		String filterString;
-		RockPropertiesFilter rockPropertiesFilter = new RockPropertiesFilter(rockProperty);
-		if (bbox == null) {
-			filterString = rockPropertiesFilter.getFilterStringAllRecords();
-		} else {
-			filterString = rockPropertiesFilter.getFilterStringBoundingBox(bbox);
-		}
+		String filterString = this.getRockPropertyFilter(rockProperty, bbox);
 		HttpRequestBase method = null;
-		
+
 		try {
-			method = this.generateWFSRequest(serviceUrl, "scalar_results", null, filterString, maxFeatures, null, ResultType.Results);
+			method = this.generateWFSRequest(serviceUrl, "scalar_results", null, filterString, maxFeatures, null,
+					ResultType.Results);
 			String responseGML = this.httpServiceCaller.getMethodResponseAsString(method);
 			String responseKML = this.wfsToKmlTransformer.convert(responseGML, serviceUrl);
 			return new WFSTransformedResponse(responseGML, responseKML, method);
@@ -53,15 +48,26 @@ public class RockPropertiesService extends BaseWFSService {
 			throw new PortalServiceException(method, e);
 		}
 
+	}
 
+	public WFSCountResponse getRockPropertiesCount(String serviceUrl, String rockProperty, int maxFeatures,
+			FilterBoundingBox bbox) throws Exception {
+		String filterString = this.getRockPropertyFilter(rockProperty, bbox);
+		HttpRequestBase method = null;
+		method = generateWFSRequest(serviceUrl, "scalar_results", null, filterString, maxFeatures, null,
+				ResultType.Hits);
+		return getWfsFeatureCount(method);
 	}
 
 	public String getRockPropertyFilter(String rockProperty, FilterBoundingBox bbox) {
 		RockPropertiesFilter filter = new RockPropertiesFilter(rockProperty);
-		return filter.getFilterStringBoundingBox(bbox);
+		if (bbox == null) {
+			return filter.getFilterStringAllRecords();
+		} else {
+			return filter.getFilterStringBoundingBox(bbox);
+		}
 	}
 
-	
 	/**
 	 * @param serviceUrl
 	 * @param type
@@ -70,17 +76,19 @@ public class RockPropertiesService extends BaseWFSService {
 	 * @return
 	 * @throws Exception
 	 */
-	public InputStream downloadRockProperties(String serviceUrl, String type, String filterString, String outputFormat) throws Exception {
-		  HttpRequestBase method = null;
-	        try {
+	public InputStream downloadRockProperties(String serviceUrl, String type, String filterString, String outputFormat)
+			throws Exception {
+		HttpRequestBase method = null;
+		try {
 
-	            method = generateWFSRequest(serviceUrl, type, null, filterString, null, null, ResultType.Results, outputFormat);
-	            return httpServiceCaller.getMethodResponseAsStream(method);
+			method = generateWFSRequest(serviceUrl, type, null, filterString, null, null, ResultType.Results,
+					outputFormat);
+			return httpServiceCaller.getMethodResponseAsStream(method);
 
-	        } catch (Exception ex) {
-	            throw new PortalServiceException(method, "Error when attempting to download from:" + serviceUrl, ex);
-	        }
-		
+		} catch (Exception ex) {
+			throw new PortalServiceException(method, "Error when attempting to download from:" + serviceUrl, ex);
+		}
+
 	}
 
 }
