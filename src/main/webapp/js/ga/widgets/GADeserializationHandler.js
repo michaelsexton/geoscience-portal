8917a1a4e37d98c50e22da2cb37e46f29ef06099
Geng Liang <geng.liang@ga.gov.au>
2016-07-01 15:10:34 +1000

    GPT-134 message reformat

--- src/main/webapp/js/ga/widgets/GADeserializationHandler.js
+++ src/main/webapp/js/ga/widgets/GADeserializationHandler.js
@@ -10,13 +10,15 @@ Ext.define('ga.widgets.GADeserializationHandler', {
         if (this.mapStateSerializer.serializedLayers.length > 0)
         {
             var restoreMsg = Ext.MessageBox.show({
-                title: 'Restoring map to its previous state...',
-                msg: 'Please wait... Restoring Featured Layers to your map. Custom Layers will not be reloaded. Click "CLEAR MAP" to start with an empty map.'
+                title: '<p style="font-size:140%;margin:0em;">Restoring map to its previous state...</p>',
+                msg: '<b><h1 style="margin:0em;">Please wait...</h2></b><br>' +
+                     '<p style="font-size:140%;margin:0em;">Restoring <b>Featured Layers</b> to your map</p><br>' +
+                     '<p style="font-size:140%;margin:0em;">Click "CLEAR MAP" to start with an empty map</p>'
             });
 
             setTimeout(function(){
                 restoreMsg.close();
-            }, 3000);
+            }, 7000);
          }
 
         this.callParent(arguments);
