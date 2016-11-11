d1990a5a06b5ce67af94d94460fb418817702700
heyagosper <heya.gosper@ga.gov.au>
2016-11-11 12:46:31 +1100

    add powered by auscope logo and text to header

--- src/main/webapp/js/ga/widgets/GAHeader.js
+++ src/main/webapp/js/ga/widgets/GAHeader.js
@@ -218,7 +218,18 @@ Ext.define('ga.widgets.GAHeader', {
                 height: '59px',
                 autoEl: {tag : 'span'}   
         };
-        
+
+        // search panel contains the search controls
+        var poweredByPanel = {
+                id: 'powered-by-logo',
+                height: '30px',
+                xtype: 'box',
+                autoEl: {
+                    tag: 'span',
+                    html: 'Powered by:'
+                }
+        };
+
         // search panel contains the search controls
         var searchPanel = {
                 id: 'search-controls',
@@ -234,7 +245,7 @@ Ext.define('ga.widgets.GAHeader', {
                 }  
         };
         
-        // north panel contains the 'Contact Us' and 'Skip to content' links
+        // south panel contains the 'Contact Us' and 'Skip to content' links
         var linksPanel = {
             id : "ga-header-south-panel-links",
             height: '40px',
@@ -252,13 +263,13 @@ Ext.define('ga.widgets.GAHeader', {
             }]    
         };
 
-        // center panel contains the search controls and the links
+        // center panel contains the "powered by Auscope" logo, search controls and the links
         var centerPanel = {
                 id: 'wrapper-search-and-links',
                 xtype: 'panel',
                 height: '30px',
                 layout: 'hbox',
-                items: [searchPanel, linksPanel]
+                items: [poweredByPanel, searchPanel, linksPanel]
             };
         
         // south panel contains the menu bar
