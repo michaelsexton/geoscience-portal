30e1b34b9a683288cc2486b078b38ba8d60de582
michaelsexton <michael.sexton@ga.gov.au>
2017-07-10 16:20:17 +1000

    Add tests

---
+++ src/test/java/au/gov/geoscience/portal/services/vocabularies/TestMineStatusVocabService.java
@@ -0,0 +1,8 @@
+package au.gov.geoscience.portal.services.vocabularies;
+
+import org.auscope.portal.core.test.PortalTestClass;
+
+public class TestMineStatusVocabService extends PortalTestClass {
+
+    /* Add Tests */
+}
---
+++ src/test/java/au/gov/geoscience/portal/services/vocabularies/TestReserveCategoryVocabService.java
@@ -0,0 +1,8 @@
+package au.gov.geoscience.portal.services.vocabularies;
+
+import org.auscope.portal.core.test.PortalTestClass;
+
+public class TestReserveCategoryVocabService extends PortalTestClass {
+
+    /* Add Tests */
+}
